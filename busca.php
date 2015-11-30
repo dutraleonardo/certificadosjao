853d4d108340dd664efbbdcf591181286c3b15b3
Leonardo Dutra <leonardo.uece@gmail.com>
2015-11-30 16:56:30 -0200

    Projeto terminado sem exibicao de templates de erro e de exibicao de dados

--- busca.php
+++ busca.php
@@ -1,18 +1,42 @@
 <?php
 include 'conexao.php';
 
-$busca = $_POST['palavra'];// palavra que o usuario digitou
-
-$busca_query = mysql_query("SELECT * FROM certificados WHERE nome LIKE '%$busca%'")or die('FLAG 1');//faz a busca com as palavras enviadas
-
+$busca = $_POST['nome'];// nome que o usuario digitou
+$buca = strtoupper($busca);
+$busca_query = mysql_query("SELECT * FROM certificacoes WHERE nome LIKE '%$busca%'")or die('Nome não encontrado! =(');//faz a busca com o nome enviado
 if (empty($busca_query)) { //Se nao achar nada, lança essa mensagem
     echo "Nenhum registro encontrado.";
 }
 
-// quando existir algo em '$busca_query' ele realizará o script abaixo.
-while ($dados = mysql_fetch_array($busca_query)) { 
-    echo "Nome: $dados[nome]<br />";
-    echo "Certificado(s): <a href='$dados[url]'; target='_blank'>CLICK AQUI!</a><br />";
-    echo "<hr>";
+$dados = mysql_fetch_assoc($busca_query);
+$nome = $dados['nome'];
+$participacao = $dados['url_participacao'];
+$temalivre= $dados['url_temalivre'];
+$painelclinico = $dados['url_painelclinico'];
+$painelcientifico = $dados['url_painelcientifico'];
+$forumclinico = $dados['url_forumclinico'];
+$excecao = $dados['excessao'];
+
+if ($busca = $nome) {
+	echo "Nome: $nome <br>";
+	if ($participacao != '') {
+		echo "Certificado de Participacao: <a href='$participacao' target='_blank'>CLIQUE AQUI</a><br>";
+		if ($temalivre != '') {
+			echo "Certificado de Tema Livre: <a href='$temalivre' target='_blank'>CLIQUE AQUI</a><br>";
+			if ($painelclinico != '') {
+				echo "Certificado de Painel Clinico: <a href='$painelclinico' target='_blank'>CLIQUE AQUI</a><br>";
+				if ($painelcientifico != '') {
+					echo "Certificado de Painel Cientifico: <a href='$painelcientifico' target='_blank'>CLIQUE AQUI</a><br>";
+					if ($forumclinico != '') {
+						echo "Certificado de Forum Clinico: <a href='$forumclinico' target='_blank'>CLIQUE AQUI</a><br>";
+						if ($excessao != '') {
+							echo "Certificado Extra: <a href='$participacao' target='_blank'>CLIQUE AQUI</a><br />";
+							echo "<hr>";
+						}
+					}
+				}
+			}
+		}
+	}
 }
 ?>
--- conexao.php
+++ conexao.php
@@ -1,44 +1,9 @@
 <?php
-/**
-*  Arquivo de conexão ao MySQL usando servidor local e externo
-*/
-$n = -1;
-// Define os servidores e configurações de cada conexão
-$n++;
-$MySQL[$n]['dominios']  = array('127.0.0.1', 'localhost'); // Possíveis dominios
-$MySQL[$n]['servidor']  = '127.0.0.1'; // Servidor MySQL
-$MySQL[$n]['usuario']   = 'root'; // Usuário MySQL
-$MySQL[$n]['senha']     = ''; // Senha MySQL
-$MySQL[$n]['banco']     = 'jaoodontou3'; // Banco de dados
-$MySQL[$n]['persis']    = false; // Conexão persistente?
-$n++;
-$MySQL[$n]['dominios']  = array('jaoodontounifor.com', 'jaoodontounifor.com/certificados');
-$MySQL[$n]['servidor']  = '127.0.0.1'; // Servidor MySQL
-$MySQL[$n]['usuario']   = 'root'; // Usuário MySQL
-$MySQL[$n]['senha']     = ''; // Senha MySQL
-$MySQL[$n]['banco']     = 'jaoodontou3'; // Banco de dados
-$MySQL[$n]['persis']    = false; // Conexão persistente?
-// Decide qual conexão usar
-foreach ($MySQL as $key=>$servidor) {
-    if (!isset($_SERVER['HTTP_HOST'])) {
-        $usar = $key;
-        break;
-    } else {
-        $encontrado = false;
-        foreach ($servidor['dominios'] as $dominio) {
-            if (strpos($_SERVER['HTTP_HOST'], $dominio) !== false) {
-              $usar = $key;
-              $encontrado = true;
-              break;
-            }
-        }
-        if ($encontrado)
-            break;
-    }
-}
-// Decide o tipo de conexão
-$MySQL['conexao'] = ($MySQL[$usar]['persis']) ? 'mysql_pconnect' : 'mysql_connect';
-// Conecta-se ao servidor usando o tipo de conexão definido
-$MySQL['link'] = $MySQL['conexao']($MySQL[$usar]['servidor'], $MySQL[$usar]['usuario'], $MySQL[$usar]['senha']) or die("Não foi possível se conectar ao servidor MySQL no endereço [".$MySQL[$usar]['servidor']."]");
-// Conecta-se ao banco de dados
-mysql_select_db($MySQL[$usar]['banco'], $MySQL['link']) or die("Não foi possível conectar-se ao banco de dados [".$MySQL[$usar]['banco']."] no servidor [".$MySQL[$usar]['servidor']."]");
+$hostdb = "localhost";
+$userdb = "root";
+$passdb = "";// senha do banco de dados
+$tabledb = "jaoodontou3";// tabela do banco de dados
+
+$conecta = mysql_connect($hostdb, $userdb, $passdb) or die (mysql_error());
+@mysql_select_db($tabledb, $conecta) or die ("Erro ao conectar com o banco de dados");
+?>
